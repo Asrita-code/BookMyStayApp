c22334188c13070ff8c489fa9b6631b7ae6db7c9
Asrita-code <asritaavl@gamail.com>
2026-04-24 09:20:49 +0530

    Asrita  Added UC1

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,8 @@
+class BoookMyStayApp {
+    public static void main(String[] args) {
+        String appName = "Book My Stay";
+        String version = "v1.0";
+        System.out.println("Welcome to " + appName);
+        System.out.println("Version: " + version);
+    }
+}
